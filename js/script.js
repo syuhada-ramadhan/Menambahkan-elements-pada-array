ab6fc4607b1f672de152f4f8332ef2104df74897
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-23 10:40:29 +0700

    Latihan menambahkan element pada array selesai

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// array hewan
+const hewan = ["Kambing", "Kerbau", "Sapi"];
+
+// method splice()
+hewan.splice(2, 0, "Domba");
+
+// method unshift()
+hewan.unshift("Kucing");
+
+// method push()
+hewan.push("Kelinci");
+
+// cetak array hewan
+console.log(hewan);
